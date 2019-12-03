145291f6dde060acfbf8e83355e67bf875b1f53a
Perry Raskin <perryraskin@gmail.com>
2019-12-02 21:56:59 -0500

    Add log statements

--- main.py
+++ main.py
@@ -291,12 +291,15 @@ def main():
   # log all errors
   dp.add_error_handler(error)
 
+  print("[bot] starting to poll for messages...")
   # Start the Bot
   updater.start_polling()
+  print("[bot] successfully polling")
 
   # Run the bot until you press Ctrl-C or the process receives SIGINT,
   # SIGTERM or SIGABRT. This should be used most of the time, since
   # start_polling() is non-blocking and will stop the bot gracefully.
+  print("[bot] starting to idle indefinitely")
   updater.idle()
 
 
